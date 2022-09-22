93276a3383c706332213bcb2e385cd6b15b3e3c6
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-09-23 01:13:32 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -96,7 +96,7 @@ def fsub_button(client, message):
         except IndexError:
             pass
         return buttons
-    if FORCE_SUB_CHANNEL1 FORCE_SUB_CHANNEL2 FORCE_SUB_GROUP1 FORCE_SUB_GROUP2:
+    if FORCE_SUB_CHANNEL1 and FORCE_SUB_CHANNEL2:
         buttons = [
             [
                 InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
