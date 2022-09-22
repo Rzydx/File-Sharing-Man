b8997da2174d69aba8f3cbb6e44f1b8d6d16e568
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-09-22 23:14:35 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -2,7 +2,7 @@
 # FROM File-Sharing-Man <https://github.com/mrismanaziz/File-Sharing-Man/>
 # t.me/SharingUserbot & t.me/Lunatic0de
 
-from config import FORCE_SUB_CHANNEL1, FORCE_SUB_GROUP1, FORCE_SUB_CHANNEL2 FORCE_SUB_GROUP2
+from config import FORCE_SUB_CHANNEL1, FORCE_SUB_GROUP1, FORCE_SUB_CHANNEL2, FORCE_SUB_GROUP2
 from pyrogram.types import InlineKeyboardButton
 
 
