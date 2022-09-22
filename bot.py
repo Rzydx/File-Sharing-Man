29c3d862e0228fc0bb80a7b07cfc679bf3119f68
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-09-22 22:16:26 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -105,7 +105,7 @@ class Bot(Client):
                 if not link:
                     await self.export_chat_invite_link(FORCE_SUB_CHANNEL2)
                     link = info.invite_link
-                self.invitelink3 = link
+                self.invitelink4 = link
                 self.LOGGER(__name__).info(
                     f"FORCE_SUB_CHANNEL2 detected!\n┌ Title: {info.title}\n└ Chat ID: {info.id}\n——"
                 )
@@ -129,7 +129,7 @@ class Bot(Client):
                 if not link:
                     await self.export_chat_invite_link(FORCE_SUB_GROUP2)
                     link = info.invite_link
-                self.invitelink4 = link
+                self.invitelink3 = link
                 self.LOGGER(__name__).info(
                     f"FORCE_SUB_GROUP2 detected!\n┌ Title: {info.title}\n└ Chat ID: {info.id}\n——"
                 )
