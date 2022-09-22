ecccb2e745c7defdaead5e5c644eea72b4c91d0b
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-09-23 01:15:37 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -39,7 +39,7 @@ def start_button(client):
             ],
         ]
         return buttons
-    if FORCE_SUB_CHANNEL1 FORCE_SUB_CHANNEL2 FORCE_SUB_GROUP1 FORCE_SUB_GROUP2:
+    if FORCE_SUB_CHANNEL1 and FORCE_SUB_CHANNEL2:
         buttons = [
             [
                 InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
